c1348c094c1982253c3d155d9bda23cbd822bdcc
Hansal Shah <62453760+HansalShah007@users.noreply.github.com>
2022-07-17 15:22:23 +0530

    Minor changes in the text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid foot-con">
                 <a className="navbar-brand nav-title" href="#">
-                    <div className="title-text foot-info">Made with ❤️⚡ by Hansal Shah [19BCP043] && Jainil Patel [19BCP056]</div>
+                    <div className="title-text foot-info">Made with ❤️⚡ by Hansal Shah && Jainil Patel </div>
                 </a>
             </div>
         </nav>
